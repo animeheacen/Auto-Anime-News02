a41ac01bd2544ce8f79570876b8ac9423c70c97d
animeheacen <pavanvallu2@gmail.com>
2025-05-30 09:13:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-# config.py
+import os
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
-URL_A = "" #News RSS Feed Url 2
-#URL_B = "" #News RSS Feed Url 2
-START_PIC = "" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
-ADMINS = "" #Admin ID
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+URL_A = os.getenv("URL_A")
+START_PIC = os.getenv("START_PIC")
+MONGO_URI = os.getenv("MONGO_URI")
+ADMINS = list(map(int, os.getenv("ADMINS", "").split()))
